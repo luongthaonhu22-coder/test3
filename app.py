3277fcb8d81c98f0ca324eaef4e39f4bb1eaf328
luongthaonhu22-coder <luongthaonhu22@gmail.com>
2026-07-03 00:52:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -246,8 +246,8 @@ elif menu == "📡 Tra cứu & Gửi Email":
         
         uploaded_file = st.file_uploader("Đính kèm tệp chứng từ bổ sung (Hình ảnh, PDF):")
         
-    if st.button("🚀 PHÁT HÀNH EMAIL THÔNG BÁO"):
-    if not receiver_email:
+if st.button("🚀 PHÁT HÀNH EMAIL THÔNG BÁO"):
+if not receiver_email:
         st.error("⚠️ Bạn chưa điền địa chỉ Email người nhận!")
     else:
         with st.spinner('Đang đóng gói và gửi chứng từ...'):
